6141c3e75e228c6f27b8e150e5bb817f99368262
Seth Hamilton <sethhamilton2017@gmail.com>
2022-01-23 12:26:07 -0600

    First commit

---
+++ data/process_data.py
@@ -0,0 +1,99 @@
+import sys
+import pandas as pd
+import re
+import numpy as np
+
+
+def load_data(messages_filepath, categories_filepath):
+	#load messages dataset
+    messages = pd.read_csv('messages.csv')
+	messages.head()
+	# load categories dataset
+	categories = pd.read_csv('categories.csv')
+	categories.head()
+
+
+def clean_data(df):
+    # merge datasets
+	df = messages.merge(categories, how = 'inner', on = 'id')
+	df.head()
+	# create a dataframe of the 36 individual category columns
+	categories = df['categories'].str.split(";", expand = True)
+	categories.head()
+	# select the first row of the categories dataframe
+	row = categories.iloc[0]
+	row = row.str.split('-', expand = True)
+
+
+	# use this row to extract a list of new column names for categories.
+	# one way is to apply a lambda function that takes everything 
+	# up to the second to last character of each string with slicing
+	category_colnames = list(row[0])
+	print(category_colnames)
+	
+	#print(row)
+	# rename the columns of `categories`
+	categories.columns = category_colnames
+	categories.head()
+	
+	for column in categories:
+    	# set each value to be the last character of the string
+    	categories[column] = categories[column].apply(lambda x: x.split('-')[1] )
+    	# convert column from string to numeric
+    	categories[column] = categories[column].astype('int64')
+	categories.head()
+	
+	categories.drop(categories.index[categories['related'] == 2], inplace = True)
+	categories.related.value_counts()
+	
+	# drop the original categories column from `df`
+	df = df.drop(columns = ['categories'])
+
+	df.head()
+	# concatenate the original dataframe with the new `categories` dataframe
+	df = pd.concat([df, categories], axis = 1)
+	df.head()
+	# check number of duplicates
+	dups = len(df[df.duplicated()])
+	print(dups)
+	# drop duplicates
+	df.drop_duplicates(inplace = True)
+	# check number of duplicates
+	print(len(df[df.duplicated()]))
+
+
+def save_data(df, database_filename):
+	#saves database
+    from sqlalchemy import create_engine
+	engine = create_engine('sqlite:///DisasterMessage.db')
+	df.to_sql('DisasterMessage', engine, index=False, if_exists = 'replace')
+
+
+def main():
+    if len(sys.argv) == 4:
+
+        messages_filepath, categories_filepath, database_filepath = sys.argv[1:]
+
+        print('Loading data...\n    MESSAGES: {}\n    CATEGORIES: {}'
+              .format(messages_filepath, categories_filepath))
+        df = load_data(messages_filepath, categories_filepath)
+
+        print('Cleaning data...')
+        df = clean_data(df)
+        
+        print('Saving data...\n    DATABASE: {}'.format(database_filepath))
+        save_data(df, database_filepath)
+        
+        print('Cleaned data saved to database!')
+    
+    else:
+        print('Please provide the filepaths of the messages and categories '\
+              'datasets as the first and second argument respectively, as '\
+              'well as the filepath of the database to save the cleaned data '\
+              'to as the third argument. \n\nExample: python process_data.py '\
+              'disaster_messages.csv disaster_categories.csv '\
+              'DisasterResponse.db')
+
+
+if __name__ == '__main__':
+    main()
---
+++ models/train_classifier.py
@@ -0,0 +1,115 @@
+import sys
+import pandas as pd
+from sqlalchemy import create_engine
+import numpy as np
+import re
+import nltk
+import sqlite3
+nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('words')
+nltk.download('averaged_perceptron_tagger')
+nltk.download('maxent_ne_chunker')
+nltk.download('wordnet')
+from nltk.corpus import stopwords
+from nltk.stem.wordnet import WordNetLemmatizer
+from nltk.tokenize import word_tokenize, sent_tokenize
+from nltk import pos_tag, ne_chunk
+from nltk.stem.porter import PorterStemmer
+from nltk.stem.wordnet import WordNetLemmatizer
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer, TfidfTransformer
+from sklearn.feature_extraction import DictVectorizer
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.feature_extraction import FeatureHasher
+from sklearn.neural_network import MLPClassifier
+from sklearn.feature_extraction.text import HashingVectorizer
+from nltk import word_tokenize          
+from nltk.stem import WordNetLemmatizer 
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import classification_report, accuracy_score
+from sklearn.utils.multiclass import type_of_target
+from sklearn.metrics import f1_score
+from sklearn.model_selection import train_test_split
+
+
+
+def load_data(database_filepath):
+    engine = create_engine('sqlite:///DisasterMessage.db')
+	df = pd.read_sql('SELECT * FROM DisasterMessage', engine)
+	df = df.dropna()
+	X = df['message'] 
+	y = df.iloc[:, 4:]
+	y = y.astype('float')
+
+
+def tokenize(text):
+    def tokenize(text):
+    text = re.sub(r"[^a-zA-Z0-9]", " ", text)
+    tokens = word_tokenize(text)
+    lemmatizer = WordNetLemmatizer()
+    clean_tokens = []
+    for tok in tokens:
+        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
+        clean_tokens.append(clean_tok)
+    return tokens
+
+
+def build_model():
+	X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = .20, random_state = 42)
+    pipeline2 = Pipeline([
+        ('vect', CountVectorizer(tokenizer = tokenize)),
+        ('tfidf', TfidfTransformer()),
+        #('hashvec', HashingVectorizer(n_features=10)),
+        #('feathash', FeatureHasher()),
+        ('clf', MultiOutputClassifier(RandomForestClassifier(n_estimators=10, max_depth=45)))   
+    ])
+	pipeline2.fit(X_train, y_train)
+	y_pred = pipeline2.predict(X_test)
+	y_pred = np.array(y_pred)
+	y_test = np.array(y_test)
+	parameters = {
+    	'clf__n_estimators': [300]
+	}
+	cv2 = GridSearchCV(pipeline2, param_grid=parameters)	
+
+
+def evaluate_model(model, X_test, Y_test, category_names):
+    print(classification_report(y_test, y_pred))
+
+
+def save_model(model, model_filepath):
+    import pickle 
+	filename = 'Disaster_Pipeline.sav'
+	pickle.dump(pipeline2, open(filename, 'wb'))
+
+
+def main():
+    if len(sys.argv) == 3:
+        database_filepath, model_filepath = sys.argv[1:]
+        print('Loading data...\n    DATABASE: {}'.format(database_filepath))
+        X, Y, category_names = load_data(database_filepath)
+        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
+        
+        print('Building model...')
+        model = build_model()
+        
+        print('Training model...')
+        model.fit(X_train, Y_train)
+        
+        print('Evaluating model...')
+        evaluate_model(model, X_test, Y_test, category_names)
+
+        print('Saving model...\n    MODEL: {}'.format(model_filepath))
+        save_model(model, model_filepath)
+
+        print('Trained model saved!')
+
+    else:
+        print('Please provide the filepath of the disaster messages database '\
+              'as the first argument and the filepath of the pickle file to '\
+              'save the model to as the second argument. \n\nExample: python '\
+              'train_classifier.py ../data/DisasterResponse.db classifier.pkl')
+
+
+if __name__ == '__main__':
+    main()
